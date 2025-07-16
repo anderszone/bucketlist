9810e8b47e97ca0f13fb6f1be129af21a484ddc3
Anders Johansson <inboxanders@gmail.com>
2025-07-16 15:35:20 +0200

    Rendered the dream list dynamically

--- dist/pages/dashboard.js
+++ dist/pages/dashboard.js
@@ -1,10 +1,53 @@
-"use strict";
 document.addEventListener("DOMContentLoaded", function () {
     var _a;
+    const dreamList = document.querySelector(".dream-list");
     const displayedUsername = document.querySelector("#user-name");
     const name = (_a = localStorage.getItem("username")) !== null && _a !== void 0 ? _a : "Guest";
     if (displayedUsername) {
         displayedUsername.innerHTML = name;
     }
+    const dreams = [{
+            id: 1,
+            name: "Lära mig HTML/CSS",
+            theme: "teknikdrömmar",
+            checked: true
+        },
+        {
+            id: 2,
+            name: "Lära mig TypeScript",
+            theme: "teknikdrömmar",
+            checked: false
+        },
+        {
+            id: 3,
+            name: "En dröm som tar flera rader lorem ipsum",
+            theme: "vardagsdrömmar",
+            checked: false
+        }
+    ];
+    for (let dream of dreams) {
+        const li = document.createElement("li");
+        li.className = "dream-list_item";
+        const input = document.createElement("input");
+        input.className = "dream-check";
+        input.type = "checkbox";
+        input.name = "dream-check";
+        input.id = `dream-check-${dream.id}`;
+        input.checked = dream.checked;
+        const label = document.createElement("label");
+        label.htmlFor = `dream-check-${dream.id}`;
+        label.innerHTML = `${dream.name}, <span class="dream-theme">${dream.theme}</span>`;
+        const button = document.createElement("button");
+        button.type = "button";
+        const image = document.createElement("img");
+        image.src = "../assets/images/trash_delete.png";
+        button.appendChild(image);
+        li.appendChild(input);
+        li.appendChild(label);
+        li.appendChild(button);
+        dreamList === null || dreamList === void 0 ? void 0 : dreamList.appendChild(li);
+        console.log("test");
+    }
 });
+export {};
 //# sourceMappingURL=Dashboard.js.map
--- src/pages/Dashboard.ts
+++ src/pages/Dashboard.ts
@@ -1,8 +1,60 @@
-document.addEventListener("DOMContentLoaded", function () {    
+import { Dream } from "../models/Dream";
+import { themes } from "../utils/variables";
+
+document.addEventListener("DOMContentLoaded", function () {  
+    const dreamList = document.querySelector(".dream-list");
     const displayedUsername = document.querySelector("#user-name");
     const name = localStorage.getItem("username") ?? "Guest";
 
     if (displayedUsername) {
         displayedUsername.innerHTML = name;
+    }
+
+    const dreams: Dream[] = [{
+        id: 1,
+        name: "Lära mig HTML/CSS",
+        theme: "teknikdrömmar",
+        checked: true
+    },
+    {
+        id: 2,
+        name: "Lära mig TypeScript",
+        theme: "teknikdrömmar",
+        checked: false
+    },
+    {
+        id: 3,
+        name: "En dröm som tar flera rader lorem ipsum",
+        theme: "vardagsdrömmar",
+        checked: false
+    }
+    ]
+
+    for (let dream of dreams) {
+        const li = document.createElement("li");
+        li.className = "dream-list_item";
+        
+        const input = document.createElement("input");
+        input.className = "dream-check";
+        input.type = "checkbox";
+        input.name = "dream-check";
+        input.id = `dream-check-${dream.id}`;
+        input.checked = dream.checked;
+
+        const label = document.createElement("label");
+        label.htmlFor = `dream-check-${dream.id}`;
+        label.innerHTML = `${dream.name}, <span class="dream-theme">${dream.theme}</span>`;
+
+        const button = document.createElement("button");
+        button.type = "button";
+
+        const image = document.createElement("img");
+        image.src = "../assets/images/trash_delete.png";
+
+        button.appendChild(image);
+        li.appendChild(input);
+        li.appendChild(label);
+        li.appendChild(button);
+        dreamList?.appendChild(li);
     }
 });
